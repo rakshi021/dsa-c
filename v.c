e9c61fc5c2104e00d4375dfc7f1b4fcbe184d2d9
Rakshi Jena <jenarakshi@gmail.com>
2025-10-17 23:41:47 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[20] = "Ram";   // String variable
+    int age = 32;             // Integer variable
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+
+    return 0;
+}
