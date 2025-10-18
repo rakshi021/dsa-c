4c1b742820fdc589a24bce94ec7ab69e23e2f99d
Rakshi Jena <jenarakshi@gmail.com>
2025-10-18 23:39:16 +0530

    .

--- v.c
+++ v.c
@@ -3,9 +3,11 @@
 int main() {
     char name[20] = "Ram";   // String variable
     int age = 32;             // Integer variable
+    char DOB[10] = "01-01-1000";
 
     printf("Name: %s\n", name);
     printf("Age: %d\n", age);
+    printf("DOB: %s\n", DOB);
 
     return 0;
 }
